3dc05f8ec29c56e7ded9217b949713153472fc14
Alex Napitupulu <sleigner@Alexs-MacBook-Pro.local>
2019-08-17 14:22:51 -0400

    feat(database): terminates process if it fails to connect to the db

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,19 @@
 import {Sequelize} from 'sequelize-typescript';
+import {logger} from './logger';
 
 const database = new Sequelize({
   url: process.env.DATABASE_URL,
   modelPaths: [__dirname + '/models/database']
 });
+
+database.authenticate()
+  .then(() => {
+    logger.info('Successfully connected to database');
+  })
+  .catch((err: Error) => {
+    logger.error('Failed to connect to database... terminate process');
+    process.exit(1);
+  });
 
 export {
   database,
